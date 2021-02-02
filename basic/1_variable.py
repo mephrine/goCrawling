89a20addf3d9895da00c7f7e4d942b828409b60f
swjung0405@sptek.co.kr <swjung0405@sptek.co.kr>
2021-02-02 17:37:47 +0900

    파이썬공부

---
+++ basic/1_variable.py
@@ -0,0 +1,23 @@
+'''
+문단주석
+'''
+
+# int 는 숫자
+stock_price = 5000
+print("stock_price : %s" % stock_price)
+print(type(stock_price))
+
+# float 소수점을 가진 숫자!
+stock_earning = 3.7
+print("stock_earning : %s" % stock_earning)
+print(type(stock_earning))
+
+# str 은 문자
+stock_name = "문자입니다."
+print("stock_name : %s" % stock_name)
+print(type(stock_name))
+
+# bool (true,false)
+stock_boolean = True
+print("stock_boolean : %s" % stock_boolean)
+print(type(stock_boolean))
---
+++ basic/2_math.py
@@ -0,0 +1,18 @@
+'''
+덧셈, 뺄셈, 곱셈, 나눗셈
+'''
+
+#int 더하기
+stock_price1 = 1000
+stock_price2 = 2000
+
+stock_price_total = stock_price1 + stock_price2
+print("%s" % stock_price_total)
+
+#int 빼기
+stock_price_minus1 = stock_price1 - stock_price2
+print("%s" % stock_price_minus1)
+
+#int 곱하기
+stock_price_multiple = stock_price1 * stock_price2
+print("%s" % stock_price_multiple)
